ab78f78753248455efa989414f08892aca446778
Marc Grunberg <marc.grunberg@unistra.fr>
2017-10-20 14:52:51 +0200

    more readable

--- scripts/get_channels.sh
+++ scripts/get_channels.sh
@@ -1,1 +1,3 @@
-curl -s http://localhost/api/v1/channels/availability.json | grep '"id":' | tr -d ' ' |  tr -d '"' |  tr -d ',' | cut -d":" -f2 | awk -F . '{print $1"_"$2}' | sort -u
+curl -s http://localhost/api/v1/channels/availability.json | \
+	grep '"id":' | tr -d ' ' |  tr -d '"' |  tr -d ',' | cut -d":" -f2 | \
+	awk -F . '{print $1"_"$2}' | sort -u
